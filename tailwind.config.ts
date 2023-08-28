8301f935d43fe2e9d507a19c912f934c00f2bb99
Van Tran <van.tran@asnet.com.vn>
2023-08-28 21:47:13 +0700

    Setup common styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#307DB8',
+        secondary: '#FACD66',
+        light: '#EFEEE0',
+        dark: '#1D2123',
+        alt: '#A4C7C6',
+        darkAlt: '#1A1E1F'
       },
-    },
+      fontSize: {
+        xs: '10px',
+        sm: '12px',
+        base: '14px',
+        lg: '17px',
+        xl: '20px',
+        '2xl': '24px',
+        '3xl': '35px'
+      }
+    }
   },
-  plugins: [],
-}
-export default config
+  plugins: []
+};
+export default config;
